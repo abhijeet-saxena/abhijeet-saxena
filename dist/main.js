689076838ce00081896d1f11d6859b694f74908d
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2021-09-10 14:59:14 +0530

    Generate dist

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('a[href^="#"]').forEach(e=>{e.addEventListener("click",function(e){e.preventDefault(),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"})})});
